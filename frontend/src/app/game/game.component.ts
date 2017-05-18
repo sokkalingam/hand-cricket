2860f59ed2158324d4c018d356a1d12f91d8a16e
Sokkalingam Subramanian <sokkalingam.subramanian@amadeus.com>
2017-05-18 00:25:22 -0400

    Basic Game with score, balls played, OUT message

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'game',
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
 })
 
 export class GameComponent {
+  userInput: number;
+  computerInput: number;
+  status: string;
 
+  noOfBallsPlayed: number = 0;
+  runsScored: number = 0;
+
+  noOfOutputs: number = 7;
+
+  /**
+  * Returns a random number between 0 and 6, both included
+  */
+  getRandomNumber(): number {
+    return Math.floor(Math.random() * this.noOfOutputs);
+  }
+
+  play(): void {
+    this.computerInput = this.getRandomNumber();
+
+    if (this.computerInput == this.userInput) {
+        this.status = "OUT";
+        this.runsScored = 0;
+        this.noOfBallsPlayed = 0;
+    } else {
+      this.status = undefined;
+      this.addRuns(this.userInput);
+      this.addBallsPlayed();
+    }
+  }
+
+  addRuns(run: number): void {
+    this.runsScored += run;
+  }
+
+  addBallsPlayed(): void {
+    this.noOfBallsPlayed++;
+  }
 }
