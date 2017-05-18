065a20c8ef3eeb2c8da4dff18b19a5516fab0b0a
Sokkalingam Subramanian <sokkalingam.subramanian@amadeus.com>
2017-05-17 23:31:35 -0400

    Hand Cricket Try Out on Angular 2

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Hand Cricket'; }
---
+++ frontend/src/app/game/game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'game',
+  templateUrl: './game.component.html'
+})
+
+export class GameComponent {
+
+}
