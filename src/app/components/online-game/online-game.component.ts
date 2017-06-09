ce9c0036269e44a3e0ea111ac25aeb4575225d8b
Sokkalingam Subramanian <lings24@gmail.com>
2017-06-09 16:48:34 -0400

    Try Out / Send and Receive messages private and group

--- src/app/components/online-game/online-game.component.ts
+++ src/app/components/online-game/online-game.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Player } from '../../model/Player';
+import { PlayerType } from '../../enum/PlayerType';
 
 var SockJS = require('sockjs-client');
 var Stomp = require('stompjs');
@@ -9,37 +12,36 @@ var Stomp = require('stompjs');
 })
 
 export class OnlineGameComponent implements OnInit {
+  player: Player;
+
   stompClient: any;
   name: string = '';
   messages: string[] = [];
 
   ngOnInit(): void {
+    this.player = new Player(PlayerType.User);
     this.connect();
   }
 
   send(): void {
-    this.stompClient.send('/app/hello', {}, JSON.stringify({ 'name': this.name }));
+    this.stompClient.send(`/app/game/ABCDE/${this.player.id}`, {}, JSON.stringify({ 'name': this.name }));
     this.name = '';
   }
 
   connect(): void {
     var that = this;
-    var socket = new SockJS('http://localhost:8080/gs-guide-websocket');
+    var socket = new SockJS('http://localhost:8080/socket-registration');
     this.stompClient = Stomp.over(socket);
     console.log('StompClient: ' + JSON.stringify(this.stompClient));
     this.stompClient.connect({}, function (frame: any) {
         console.log('Connected: ' + frame);
-        that.stompClient.subscribe('/topic/greetings/GAME', function (greeting: any) {
+        that.stompClient.subscribe(`/live-updates/ABCDE/${that.player.id}`, function (greeting: any) {
             console.log('Received Greeting: ' + JSON.stringify(greeting));
-            that.messages.push(JSON.parse(greeting.body).content);
+            that.messages.push(greeting.body);
         });
-        that.stompClient.subscribe('/topic/greetings/USER', function (greeting: any) {
+        that.stompClient.subscribe(`/live-updates/ABCDE`, function (greeting: any) {
             console.log('Received Greeting: ' + JSON.stringify(greeting));
-            that.messages.push(JSON.parse(greeting.body).content);
-        });
-        that.stompClient.subscribe('/topic/greetings/COMP', function (greeting: any) {
-            console.log('Received Greeting: ' + JSON.stringify(greeting));
-            that.messages.push(JSON.parse(greeting.body).content);
+            that.messages.push(greeting.body);
         });
     }, function (err: any) {
         console.log('Error Is: ', err);
