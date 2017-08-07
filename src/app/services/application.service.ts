abbe89d944e54d3f5878ee8e3eb8c02d28747264
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2017-08-07 12:22:10 -0400

    Wake Server on Feedback page init

--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/observable';
 
 @Injectable()
 export class ApplicationService {
 
   baseUrl: string = 'https://hand-cricket-server.herokuapp.com';
   // baseUrl: string = 'http://localhost:8080';
+
+  constructor(private http: Http) {}
+
+  wakeServer(): void {
+    this._wakeUp().subscribe(
+      (str: string) => console.log('wakeServer: ' + str),
+      (error) => console.log('Could not wake server: ' + error)
+    );
+  }
+
+  _wakeUp(): Observable<String> {
+    return this.http.get(this.baseUrl)
+      .map((response: Response) => {
+        console.log('Server WakeUp Call: ' + JSON.stringify(response));
+        return response.text();;
+      });
+  }
 
 }
