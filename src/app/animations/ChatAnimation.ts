169582ce3f5014d1ec6246ad65a21e16cbb2bc4c
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2017-08-14 13:58:20 -0400

    Chat animation changes

--- src/app/animations/ChatAnimation.ts
+++ src/app/animations/ChatAnimation.ts
@@ -11,18 +11,18 @@ export const ChatAnimation = [
   trigger('sent', [
     transition('* => *', [
       animate(300, keyframes([
-        style({opacity: 0, transform: 'translateX(100%)', offset: 0}),
-        style({opacity: 1, transform: 'translateX(-30px)',  offset: 0.4}),
-        style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+        style({opacity: 0, transform: 'translateY(100%)', offset: 0}),
+        style({opacity: 1, transform: 'translateY(-30px)',  offset: 0.4}),
+        style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
       ]))
     ])
   ]),
   trigger('received', [
     transition('* => *', [
       animate(300, keyframes([
-        style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
-        style({opacity: 1, transform: 'translateX(30px)',  offset: 0.4}),
-        style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+        style({opacity: 0, transform: 'translateY(-100%)', offset: 0}),
+        style({opacity: 1, transform: 'translateY(30px)',  offset: 0.4}),
+        style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
       ]))
     ])
   ])
